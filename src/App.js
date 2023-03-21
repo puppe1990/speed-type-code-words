bae8782600fada021963c3a7c108399e016e82bb
puppe1990 <matheus.puppe90@hotmail.com>
2023-03-21 15:51:01 -0300

    able to edit the code

--- src/App.js
+++ src/App.js
@@ -34,6 +34,12 @@ function TyperTester() {
     setTypingProgress(0);
   };
 
+  const handleCodeEdit = (event) => {
+    setCurrentCode(event.target.textContent);
+    setUserInput("");
+    setTypingProgress(0);
+  };
+
   const handleChange = (event) => {
     setUserInput(event.target.value);
 
@@ -45,15 +51,26 @@ function TyperTester() {
     setTypingProgress(progress);
   };
 
-  const handleKeyPress = (event) => {
+  const handleStart = () => {
+    setStartTime(Date.now());
+  };
+
+  const handleKeyDown = (event) => {
     if (event.target.value === currentCode) {
       setEndTime(Date.now());
       setTimeTaken((endTime - startTime) / 1000);
     }
   };
 
-  const handleStart = () => {
-    setStartTime(Date.now());
+  const handleKeyUp = (event) => {
+    setUserInput(event.target.value);
+
+    let typedChars = event.target.value;
+    let correctChars = currentCode.slice(0, typedChars.length);
+
+    // Calculate the progress based on the number of correctly typed characters
+    let progress = (correctChars.length / currentCode.length) * 100;
+    setTypingProgress(progress);
   };
 
   return (
@@ -72,29 +89,25 @@ function TyperTester() {
             .find((language) => language.id === selectedLanguage)
             .name.toLowerCase()}
           style={darcula}
+          contentEditable={true}
+          suppressContentEditableWarning={true}
+          onInput={handleCodeEdit}
         >
           {currentCode}
         </SyntaxHighlighter>
       </div>
-      <div className="user-input">
-        <div
-          className="editable-input"
-          contentEditable={true}
-          suppressContentEditableWarning={true}
-          onInput={handleChange}
-          onKeyPress={handleKeyPress}
-          style={{ whiteSpace: "pre-wrap" }}
-        >
-          {userInput}
-        </div>
-      </div>
       <textarea
+        className="my-textarea"
         value={userInput}
         onChange={handleChange}
-        onKeyPress={handleKeyPress}
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
       />
       <div className="progress">
-        <ProgressBar now={typingProgress} label={`${typingProgress}%`} />
+        <ProgressBar
+          now={typingProgress}
+          label={`${typingProgress === 100 ? "Complete!" : typingProgress}%`}
+        />
       </div>
       <button className="start-button" onClick={handleStart}>
         START
